4b5e921f45a0ef252a017ea7254ce023ecd811a1
tbaudson <tbaudson@freenet.de>
2023-12-30 22:23:02 +0100

    After a review and no depricated code anymore

--- src/Application.java
+++ src/Application.java
@@ -1,69 +1,7 @@
-import okhttp3.*;
-import org.jetbrains.annotations.NotNull;
-import org.json.JSONObject;
-
-import java.io.IOException;
-
 public class Application {
 
-    public static final String OPENAI_API_KEY = "OPENAI_API_KEY";
-
-    public static void main(String[] args) throws IOException {
-        // Geben Sie den gewünschten Text ein, um eine Generierung anzufordern
-        String prompt = "Wie geht es dir?";
-
-        Request request = createRequestForPrompt(prompt);
-
-        // Senden Sie die Anfrage und erhalten Sie die Antwort
-        OkHttpClient client = new OkHttpClient();
-        sendQuestion(client, request);
-    }
-
-    @NotNull
-    private static Request createRequestForPrompt(String prompt) {
-        // Setzen Sie Ihren OpenAI API-Schlüssel hier ein
-        String apiKey = getApiKey();
-
-        // THis is
-
-        // Erstellen Sie eine Anfrage an die OpenAI API
-        MediaType mediaType = MediaType.parse("application/json");
-        RequestBody body = RequestBody.create(mediaType, "{\"prompt\": \"" + prompt + "\", \"max_tokens\": 100}");
-
-        return new Request.Builder()
-                .url("https://api.openai.com/v1/engines/text-davinci-003/completions")
-                .post(body)
-                .addHeader("Content-Type", "application/json")
-                .addHeader("Authorization", "Bearer " + apiKey)
-                .build();
-    }
-
-    private static void sendQuestion(OkHttpClient client, Request request) throws IOException {
-        String responseBody;
-        try (Response response = client.newCall(request).execute()) {
-            if(response == null  || response.body() == null) {
-                System.out.println("Response failed !");
-                return;
-            }
-
-            responseBody = response.body().string();
-        }
-
-        // Analysieren Sie die Antwort und drucken Sie den generierten Text
-        JSONObject jsonObject = new JSONObject(responseBody);
-        String generatedText = jsonObject.getJSONArray("choices").getJSONObject(0).getString("text");
-        System.out.println(generatedText);
-    }
-
-    private static String getApiKey() {
-        String variableValue = System.getenv(OPENAI_API_KEY);
-
-        if (variableValue != null) {
-            System.out.println("Der Wert der Variable " + OPENAI_API_KEY + " ist: " + variableValue);
-        } else {
-            System.out.println("Die Variable " + OPENAI_API_KEY + " existiert nicht oder hat keinen Wert.");
-        }
-
-        return variableValue;
+    public static void main(String[] args) {
+        MySimpleClient simpleClient = new MySimpleClient();
+        simpleClient.startRequest();
     }
 }
---
+++ src/MySimpleClient.java
@@ -0,0 +1,84 @@
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import okhttp3.*;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+public class MySimpleClient {
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
+    private static final String OPENAI_API_KEY = "OPENAI_API_KEY";
+    private static final String API_URL = "https://api.openai.com/v1/engines/text-davinci-003/completions";
+    private static final OkHttpClient client = new OkHttpClient();
+
+    public void startRequest() {
+        String prompt = "Wie geht es dir?";
+
+        Request request = createRequestForPrompt(prompt);
+
+        try {
+            sendQuestion(request);
+        } catch (IOException e) {
+            System.err.println("An error occurred while sending the question: " + e.getMessage());
+        }
+    }
+
+    @NotNull
+    private Request createRequestForPrompt(String prompt) {
+        String apiKey = getApiKey();
+
+        Gson gson = new Gson();
+        JsonObject jsonBody = new JsonObject();
+        jsonBody.addProperty("prompt", prompt);
+        jsonBody.addProperty("max_tokens", 100);
+        String jsonBodyString = gson.toJson(jsonBody);
+        MediaType mediaType = MediaType.parse("application/json");
+        RequestBody body = RequestBody.create (jsonBodyString, mediaType);
+
+        return new Request.Builder()
+                .url(API_URL)
+                .post(body)
+                .addHeader("Content-Type", "application/json")
+                .addHeader("Authorization", "Bearer " + apiKey)
+                .build();
+    }
+
+    private void sendQuestion(Request request) throws IOException {
+        String responseBody;
+        try (Response response = client.newCall(request).execute()) {
+            if (response.body() == null) {
+                logger.error("Response failed !");
+                return;
+            }
+
+            responseBody = response.body().string();
+        }
+
+        JsonObject jsonObject = JsonParser.parseString(responseBody).getAsJsonObject();
+
+        JsonArray choicesArray = jsonObject.getAsJsonArray("choices");
+        if (choicesArray != null && choicesArray.size() > 0) {
+            JsonObject firstChoice = choicesArray.get(0).getAsJsonObject();
+            String generatedText = firstChoice.get("text").getAsString();
+            logger.info(generatedText);
+        } else {
+            logger.error("No choices found in the JSON response.");
+        }
+    }
+
+    private String getApiKey() {
+        String variableValue = System.getenv(OPENAI_API_KEY);
+
+        if (variableValue == null) {
+            throw new IllegalStateException("API key not found");
+        }
+
+        logger.info("Der Wert der Variable " + OPENAI_API_KEY + " ist: " + variableValue);
+        return variableValue;
+    }
+
+}
